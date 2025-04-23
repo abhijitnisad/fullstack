8713b9b17d342b08946746f16536f5b911367352
abhijitnisad <abhijit.nisad99@gmail.com>
2025-04-23 18:15:05 +0530

    db added

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors"
+import db from "./utils/db.js"
 
 
 dotenv.config()
 
 const app = express()
 const port = process.env.PORT || 3000
+ 
+ 
+app.use(
+  cors({
+    origin: process.env.BASE_URL,
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
     res.send("Hi , This is Home Page")
+})
+app.get("/abhijit", (req, res) => {
+    res.send("Hi , This is Abhijit")
 })
 
 
 // console.log(process.env.PORT);
 
+//connect to db
+db();
 
 app.listen(port, ()=>{
     console.log(`✔️  Server is running on port :${port}`);
---
+++ utils/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const db = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Connected to mongodb");
+    })
+    .catch((err) => {
+      console.log("Error connecting mongodb" , err);
+    });
+};
+
+export default db;
+ 
